62286916ee88e0dab202e526381ec6e9eeaeaf75
MartinBenicek <63211618+MartinBenicek@users.noreply.github.com>
2024-11-21 16:44:25 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DND 5e Spells',
+  webDir: 'dist'
+};
+
+export default config;
